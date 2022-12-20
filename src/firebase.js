ecae5a64136b9cb1cb36e0ebda8316762059ec60
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-20 13:21:00 +0530

    Created firebase.js & added firebaseConfig into it

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDLPUa8cn4IENwm6EAcNC5Cg5zNNH1kFrI",
+  authDomain: "slack-clone-yt-eb639.firebaseapp.com",
+  projectId: "slack-clone-yt-eb639",
+  storageBucket: "slack-clone-yt-eb639.appspot.com",
+  messagingSenderId: "746617181168",
+  appId: "1:746617181168:web:24e8de488208a140650335",
+};
+
+const app = initializeApp(firebaseConfig);
