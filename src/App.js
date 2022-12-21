bc4471d188af1f2b3baad6cb0009b8755024b935
Saadat Badgujar <saadatbadgujar@gmail.com>
2022-12-21 11:22:49 +0530

    Added Sidebar Header Component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
 import React from "react";
 import "./App.css";
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import styled from "styled-components";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
       <Router>
-        <Routes>
-          <Route path="/" element={<Header />} />
-        </Routes>
+        <Header />
+        <AppBody>
+          <Sidebar />
+          <Routes>
+            <Route path="/" />
+          </Routes>
+        </AppBody>
       </Router>
     </div>
   );
 }
 
 export default App;
+
+const AppBody = styled.div`
+  display: flex;
+  height: 100vh;
+`;
